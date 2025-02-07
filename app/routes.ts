cc6cdf1a8de093d28204e4252b0ebaae1ba89cf4
Jainil2 <chauhanjainil305@gmail.com>
2025-02-07 09:47:10 +0530

    learn javascript files

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout, prefix } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("./api/fetch.tsx"),
+
+    // layout("./l1.tsx",[
+    //     route('about', "./information/about.tsx"),
+    //     route('contact', "./information/contact.tsx")
+    // ])
+
+    ...prefix('learn', [
+        index('./information/about.tsx'),
+        route('contact/:id/:id', './information/contact.tsx'),
+    ])
+
+] satisfies RouteConfig;
